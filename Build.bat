9d06bcf5c1ea548d3a14fe207df3a9ca8aed65eb
Lillu70 <Valtteri.Ilmari.Kois@gmail.com>
2024-03-09 14:03:20 +0200

    Build bat files

---
+++ Build.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+pushd bin
+
+set common_linker_flags= /incremental:no /opt:ref User32.lib Gdi32.lib
+set defines= /D_DB
+
+cl /Fe"Nalkapeli.exe" %defines% /Od /GR- /EHa- /W3 /nologo /Zi /std:c++17 ../src/Main.cpp ../src/App/App.cpp /link %common_linker_flags%
+popd
+
+
