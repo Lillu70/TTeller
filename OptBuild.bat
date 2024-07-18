70ceefbcf62dc2bfed9d43d0f606086a918592cd
Lillu70 <Valtteri.Ilmari.Kois@gmail.com>
2024-07-18 19:52:32 +0300

    Build scripts

--- OptBuild.bat
+++ OptBuild.bat
@@ -2,9 +2,9 @@
 cls
 pushd bin
 
-set common_linker_flags= /incremental:no /opt:ref User32.lib Gdi32.lib
-set defines= /DINSTRUMENTATION
-:: set defines=
+set common_linker_flags= /incremental:no /opt:ref User32.lib Gdi32.lib Comdlg32.lib
+:: set defines= /DINSTRUMENTATION
+set defines=
 
-cl /Fe"Nalkapeli.exe" %defines% /GR- /EHa- /W3 /options:strict /sdl /nologo /Zi /fp:fast /fp:except- /GA /O2 /std:c++17 ../src/Main.cpp ../src/App/App.cpp /link %common_linker_flags%
+cl /Fe"StoryToTheDeath.exe" %defines% /GR- /EHa- /W3 /options:strict /sdl /nologo /Zi /fp:fast /fp:except- /GA /O2 /std:c++17 ../src/Main.cpp ../src/App/App.cpp /link %common_linker_flags%
 popd
